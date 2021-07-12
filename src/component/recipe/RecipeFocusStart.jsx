885664279846ee89388a5486e0d1af9de32fe93c
Jacob Harrelson <random08@gmail.com>
2021-07-12 17:28:42 -0500

    tarted recipe focus start comp

---
+++ src/component/recipe/RecipeFocusStart.jsx
@@ -0,0 +1,28 @@
+import React, { useContext, useState } from 'react';
+import {
+  Paper, Box, Typography, TextField, Button, IconButton, Container, Hidden,
+} from '@material-ui/core';
+import Image from 'material-ui-image';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@material-ui/core/styles';
+
+import SpacingDesign from '../context/design/SpacingDesign';
+import LogStatus from '../context/auth/LogStatus';
+import backgroundBG from '../../assets/lowpoly2.png';
+
+
+
+const RecipeFocus = () =>  {
+
+  const themeDesign = useTheme();
+  const
+
+
+  return (
+
+  );
+};
+
+
+
+export default RecipeFocusStart;
