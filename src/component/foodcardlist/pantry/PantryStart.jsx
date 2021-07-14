6ba41b005224533671f198ef0b1150a86d862068
Will McCulloch <willdmcculloch@gmail.com>
2021-07-14 11:05:54 -0700

    implement pantry design with sample data

--- src/component/foodcardlist/pantry/PantryStart.jsx
+++ src/component/foodcardlist/pantry/PantryStart.jsx
@@ -1,4 +1,5 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import axios from 'axios';
 import {
  Card, CardActionArea, CardActions, CardContent, CardMedia, Paper, Box, Typography, TextField, Button, IconButton, Container, Hidden, FormLabel, FormControl, FormGroup, FormControlLabel, FormHelperText, Checkbox
 } from '@material-ui/core';
@@ -17,14 +18,62 @@ import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import SpacingDesign from '../../context/design/SpacingDesign';
 import theme from '../../context/design/ThemeDesign';
 
+const data = [
+  {
+    "name": "Lemons",
+    "image": lemonImg,
+    "selected": false
+  },
+  {
+    "name": "Cheese",
+    "image": cheeseImg,
+    "selected": false
+  }
+]
+
+
+
 const PantryStart = () => {
- const themeDesign = useTheme();
- const [filter, setFilter] = useState('');
+  const themeDesign = useTheme();
+  const [ingredients, setIngredients] = useState(data);
+  const [selected, setSelected] = useState([])
+  const [filter, setFilter] = useState('');
 
   const handleFilter = (e) => {
     setFilter(e.target.value.toLowerCase());
-    console.log(filter)
   }
+
+  const handleSelect = (e) => {
+    console.log(e.target.name)
+  }
+
+  const handleSubmit = (e) => {
+    if(filter.length < 3) {
+      console.log('not long enough');
+    } else {
+      // console.log(filter);
+
+      // axios.get(`http://localhost:8000/api/ingredients/${filter}/search`)
+      //   .then((result) => {
+      //     let img = `https://spoonacular.com/cdn/ingredients_500x500/${result.data.status.results[0].image}`;
+      //     let name = result.data.status.results[0].name;
+
+      //     let obj = {
+      //       "name": name,
+      //       "image": img,
+      //       "selected": false
+      //     }
+
+      //     setIngredients([...ingredients, obj]);
+      //   })
+      //   .catch((e) => { console.log(e) })
+    }
+
+  }
+
+  useEffect(() => {
+    //watch for change in ingredients list and update selected list
+  }, [ingredients])
 
   return (
     <Box style={{ ...SpacingDesign.marginBottom(1) }}>
@@ -44,12 +93,11 @@ const PantryStart = () => {
           ...SpacingDesign.marginBottom(1),
           ...SpacingDesign.marginTop(1),
           ...SpacingDesign.padding(0)
-
         }}
         elevation={1}>
 
         <Box display='flex' flexDirection='row-reverse' justifyContent='space-between'style={{...SpacingDesign.paddingTop(0)}}>
-          <IconButton >
+          <IconButton onClick={handleSubmit}>
             <AddCircleOutlineOutlinedIcon style={{ ...SpacingDesign.fontSize(5)}}></AddCircleOutlineOutlinedIcon>
           </IconButton>
 
@@ -63,61 +111,40 @@ const PantryStart = () => {
           My Pantry
         </Typography>
 
-        <Card style={{ ...SpacingDesign.marginy(3) }} elevation={5}>
-          <CardContent>
-            <CardMedia
-              style={{ ...SpacingDesign.height(40)}}
-              image={lemonImg}
-              title='Lemon'
-            />
-            <Box display='flex' flexDirection='column'>
-              <Typography variant='h5' align='center' style={{ ...SpacingDesign.marginTop(3) }}>
-                Lemons
-              </Typography>
-              <Box display='flex' justifyContent='space-between' style={{
-                // ...SpacingDesign.marginx(1),
-                ...SpacingDesign.marginLeft(4.5),
-                ...SpacingDesign.marginRight(2.5),
-                }}>
-                <FormControlLabel
-                  control={ <Checkbox name="select" style={{ transform: 'scale(1.5)' }} /> }
+        {data.filter(main => {
+          return (main.name.toLowerCase().indexOf(filter) !== -1)
+        })
+            .map((item) =>
+            <Card style={{ ...SpacingDesign.marginy(3) }} elevation={5}>
+              <CardContent>
+                <CardMedia
+                  style={{ ...SpacingDesign.height(40) }}
+                  image={item.image}
+                  title={item.name}
                 />
-                <IconButton >
-                  <HighlightOffIcon  style={{ ...SpacingDesign.fontSize(5) }}></HighlightOffIcon>
-                </IconButton>
-              </Box>
-            </Box>
-          </CardContent>
-        </Card>
+                <Box display='flex' flexDirection='column'>
+                  <Typography variant='h5' align='center' style={{ ...SpacingDesign.marginTop(3) }}>
+                    {item.name}
+                  </Typography>
+                  <Box display='flex' justifyContent='space-between' style={{
+                    // ...SpacingDesign.marginx(1),
+                    ...SpacingDesign.marginLeft(4.5),
+                    ...SpacingDesign.marginRight(2.5),
+                  }}>
+                    <FormControlLabel
+                      control={<Checkbox onClick={handleSelect} name={item.name} style={{ transform: 'scale(1.5)' }} />}
+                    />
+                    <IconButton >
+                      <HighlightOffIcon style={{ ...SpacingDesign.fontSize(5) }}></HighlightOffIcon>
+                    </IconButton>
+                  </Box>
+                </Box>
+              </CardContent>
+            </Card>
+          )}
 
-        <Card style={{ ...SpacingDesign.marginy(3) }} elevation={5}>
-          <CardContent>
-            <CardMedia
-              style={{ height: '300px' }}
-              image={cheeseImg}
-              title='Cheese'
-            />
-            <Box display='flex' flexDirection='column'>
-              <Typography variant='h5' align='center' style={{ ...SpacingDesign.marginTop(3) }}>
-                Cheese
-              </Typography>
-              <Box display='flex' justifyContent='space-between' style={{
-                // ...SpacingDesign.marginx(1),
-                ...SpacingDesign.marginLeft(4),
-                ...SpacingDesign.marginRight(3),
-                ...SpacingDesign.marginBottom(0),
-                ...SpacingDesign.padding(0)
-                }}>
-                <FormControlLabel
-                  control={ <Checkbox name="select" style={{ transform: 'scale(1.5)' }} /> }
-                />
-                <IconButton >
-                  <HighlightOffIcon  style={{ ...SpacingDesign.fontSize(5) }}></HighlightOffIcon>
-                </IconButton>
-              </Box>
-            </Box>
-          </CardContent>
-        </Card>
+
+
 
       </Container>
     </Box>
